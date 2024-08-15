3e06d1126f23922cc1b0948b07637af90e81db7e
Marshal10 <marshalmaleane123@gmail.com>
2024-08-15 20:27:26 +0530

    Login page complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
 import PageNav from "../components/PageNav";
+import styles from "./Login.module.css";
+import Button from "../components/Button";
 
 function Login() {
+  const [email, setEmail] = useState("admin@example.com");
+  const [password, setPassword] = useState("qwerty");
+
   return (
-    <div>
+    <main className={styles.login}>
       <PageNav />
-      <h1>Login</h1>
-    </div>
+      <form className={styles.form}>
+        <div className={styles.row}>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <div className={styles.row}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </div>
+
+        <div>
+          <Button type="primary">Login</Button>
+        </div>
+      </form>
+    </main>
   );
 }
 
