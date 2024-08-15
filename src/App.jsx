19850a540c2e269739bcc24f74d5f66629cb52bf
Marshal10 <marshalmaleane123@gmail.com>
2024-08-15 19:36:43 +0530

    Basic Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
