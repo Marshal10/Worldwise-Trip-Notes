ddd73542aba7cb327d4237cfef8b8e6a295af60b
Marshal10 <marshalmaleane123@gmail.com>
2024-08-15 20:07:39 +0530

    Pricing and Product page complete

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,11 +1,29 @@
 import PageNav from "../components/PageNav";
+import styles from "./Pricing.module.css";
 
 function Pricing() {
   return (
-    <div>
+    <main className={styles.pricing}>
       <PageNav />
-      <h1>Pricing</h1>
-    </div>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            id dolores doloremque beatae minima eaque maiores ipsam enim
+            repellendus mollitia.
+          </p>
+        </div>
+        <img
+          src="../public/img-2.jpg"
+          alt="overview of a large city with skyscrapers"
+        />
+      </section>
+    </main>
   );
 }
 
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,30 @@
 import PageNav from "../components/PageNav";
+import styles from "./Pricing.module.css";
 
 function Product() {
   return (
-    <div>
+    <main className={styles.pricing}>
       <PageNav />
-      <h1>Product</h1>
-    </div>
+      <section>
+        <img
+          src="../public/img-1.jpg"
+          alt="person with dog overlooking mountain with sunset"
+        />
+        <div>
+          <h2>About WorldWise.</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam,
+            temporibus. Ab quis iure tenetur voluptate earum. Quibusdam,
+            repudiandae? Voluptate officia, numquam et iusto porro accusamus
+            perspiciatis.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente
+            quaerat tenetur, quibusdam laboriosam doloribus tempore! Minus eius
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
 
