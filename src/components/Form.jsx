553a711f11f80765e66ebe17a470ffede15e9af4
Marshal10 <marshalmaleane123@gmail.com>
2024-08-17 14:53:57 +0530

    Displaying map

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
 import BackButton from "./BackButton";
 import Button from "./Button";
 import styles from "./Form.module.css";
 import DatePicker from "react-datepicker";
 
 function Form() {
+  const [cityName, setCityname] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [notes, setNotes] = useState("");
+
   return (
     <form className={styles.form}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
-        <input type="text" id="cityName"></input>
+        <input
+          type="text"
+          id="cityName"
+          value={cityName}
+          onChange={(e) => setCityname(e.target.value)}
+        ></input>
         <span className={styles.flag}>Emoji</span>
       </div>
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to city?</label>
-        <DatePicker id="date" dateFormat="dd/MM/yyyy" />
+        <DatePicker
+          id="date"
+          dateFormat="dd/MM/yyyy"
+          selected={date}
+          onChange={(date) => setDate(date)}
+        />
       </div>
 
       <div className={styles.row}>
         <label htmlFor="notes">Notes about your trip to city</label>
-        <textarea id="notes"></textarea>
+        <textarea
+          id="notes"
+          value={notes}
+          onChange={(e) => setNotes(e.target.value)}
+        ></textarea>
       </div>
 
       <div className={styles.buttons}>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,33 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
+
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPos, setMapPos] = useState([40, 0]);
   const navigate = useNavigate();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <p>Map</p>
-      <p>Latitude:{lat}</p>
-      <p>Longitude:{lng}</p>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPos}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPos}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
